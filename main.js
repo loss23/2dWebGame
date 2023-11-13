111d1fba543748d08e88b8b8659298f2feacf850
loss23 <61843498+loss23@users.noreply.github.com>
2023-11-13 14:40:39 -0800

    a

--- main.js
+++ main.js
@@ -2,21 +2,25 @@ var canv = document.getElementById("Canvas")
 var ctx = canv.getContext("2d");
 
 let MapRules = {
-    w: 1850,
-    h: 1500,
+    w: 1922,
+    h: 1650,
 }
 let plr = {
-    pos: { x: MapRules.w/9.5, y: MapRules.h/11.5 },
-    size: {w: 40, h: 35}
+    pos: { x: 0, y: 0 },
+    size: {w: 40, h: 35},
+    MoveAmount: MapRules.w / 50
 };
 
+plr.size.x = MapRules.w / 50
+plr.size.y = MapRules.h / 50
+
 function FillRow(x, y, CurColor) {
-    for (let i = 0; i < MapRules.w / 55; i++) {
+    for (let i = 0; i < MapRules.w / 50; i++) {
 
         ctx.fillStyle = CurColor;
-        ctx.fillRect(x, y, MapRules.w / 55, MapRules.h / 50)
+        ctx.fillRect(x, y, MapRules.w / 50, MapRules.h / 50)
 
-        x += MapRules.w / 60
+        x += MapRules.w / 50
 
         if (CurColor == "#77eb34") {
             CurColor = "#6bd92b"
@@ -30,15 +34,13 @@ function DrawCharacter() {
     ctx.fillStyle = "LightBlue";
     ctx.fillRect(plr.pos.x, plr.pos.y, plr.size.w, plr.size.h);
     ctx.fillStyle = "Black";
-    ctx.fillRect(plr.pos.x+2.5, plr.pos.y+2.5, plr.size.w/5, plr.size.h/6); // Left Eye
-    ctx.fillRect(plr.pos.x+30, plr.pos.y+2.5, plr.size.w/5, plr.size.h/6); // Right Eye
+    ctx.fillRect(plr.pos.x+2.5, plr.pos.y+5, plr.size.w/5, plr.size.h/6); // Left Eye
+    ctx.fillRect(plr.pos.x+22, plr.pos.y+5, plr.size.w/5, plr.size.h/6); // Right Eye
 
-    ctx.fillRect(plr.pos.x+2.5, plr.pos.y+15, plr.size.w/1.1, plr.size.h/4.5); // Mouth
-    
-    ctx.fillStyle = "#943600";
-    ctx.fillRect(plr.pos.x+2.5, plr.pos.y+15, plr.size.w/1.2, plr.size.h/4.5); // Hat
+    ctx.fillRect(plr.pos.x+2.5, plr.pos.y+15, plr.size.w/1.2, plr.size.h/4.5); // Mouth
 }
 function DrawMap() {
+    ctx.clearRect(0,0,MapRules.w,MapRules.h)
     ctx.fillStyle = "Green";
     ctx.fillRect(0, 0, MapRules.w, MapRules.h)
 
@@ -65,7 +67,30 @@ DrawCharacter();
 //Movement
 document.addEventListener("keydown", function (e) {
     if (e.key == "ArrowRight") {
-        plr.pos.x += 5;
+        plr.pos.x += plr.MoveAmount;
+        DrawMap();
         DrawCharacter()
+    }
+    if (e.key == "ArrowLeft") {
+        plr.pos.x -= plr.MoveAmount;
+        DrawMap();
+        DrawCharacter()
+    }
+    if (e.key == "ArrowDown") {
+        plr.pos.y += plr.MoveAmount;
+        DrawMap();
+        DrawCharacter()
+    }
+    if (e.key == "ArrowUp") {
+        plr.pos.y -= plr.MoveAmount;
+        DrawMap();
+        DrawCharacter()
+    }
+
+    if (e.key == " ") {
+        DrawMap();
+        DrawCharacter()
+
+        ctx.drawImage(document.getElementById("swordSwingGif"),plr.pos.x,plr.pos.y)
     }
 });
